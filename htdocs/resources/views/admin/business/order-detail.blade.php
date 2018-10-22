64fd2da5ef597fa8236ca97e98f454e12e669b07
yong <813480776@qq.com>
2018-10-22 14:49:28 +0800

    adminorder

--- htdocs/resources/views/admin/business/order-detail.blade.php
+++ htdocs/resources/views/admin/business/order-detail.blade.php
@@ -182,10 +182,10 @@
 
 	<script>
 		
-        var html = "<html><head><meta charset='utf-8' /></head><body>" + document.getElementsById("biaoge").outerHTML + "</body></html>";
+        var html = "<html><head><meta charset='utf-8' /></head><body>" + document.getElementById("biaoge").outerHTML + "</body></html>";
         // 实例化一个Blob对象，其构造函数的第一个参数是包含文件内容的数组，第二个参数是包含文件类型属性的对象
         var blob = new Blob([html], { type: "application/vnd.ms-excel" });
-        var a = document.getElementsById("daochu");
+        var a = document.getElementById("daochu");
         // 利用URL.createObjectURL()方法为a元素生成blob URL
         a.href = URL.createObjectURL(blob);
         // 设置文件名

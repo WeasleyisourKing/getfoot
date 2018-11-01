51fc0c8e7df659afdae87251d04592043c0c194e
coen <chen.bao@yn-pulse.com>
2018-11-01 14:12:05 +0800

    cc

--- htdocs/resources/views/layouts/app.blade.php
+++ htdocs/resources/views/layouts/app.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/image/favicon_1.ico">
+    <link rel="shortcut icon" href="/uploads/12buy.ico">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-    <title>Admin - Pulse</title>
+    <title>12buy商城管理系统</title>
 
     <!-- Styles -->
     <link href="{{ asset('lib/css/bootstrap.min.css') }}" rel="stylesheet">

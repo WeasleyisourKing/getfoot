52fc9816827a372f48be7b3eaaa4a8066da2f09b
yong <813480776@qq.com>
2019-01-14 15:25:23 +0800

    admin

--- htdocs/resources/views/web/layout/app.blade.php
+++ htdocs/resources/views/web/layout/app.blade.php
@@ -449,6 +449,7 @@
 	var CartNumber=function(){
 		var shopCartLength=localStorage.getItem("myCart") ? JSON.parse(localStorage.getItem("myCart")) : '';
 		console.log(shopCartLength)
+		console.log(shopCartLength.length)
 		if(shopCartLength.length<1 ){
 			// console.log("1121")
 			$("#CartNumber").hide();

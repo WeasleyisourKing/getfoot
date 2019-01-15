9fceae7c2669547ede7a2d39a7beac6e192d7e8c
mac <mac@macdeMacBook-Air-5.local>
2019-01-15 14:27:46 +0800

    admin

--- htdocs/resources/views/web/personal.blade.php
+++ htdocs/resources/views/web/personal.blade.php
@@ -974,7 +974,7 @@
 									<div class="am-u-sm-2 myOderDetailText">
 										<p>${products[i].count}
 											<br>
-											<span>${Sprice1(product[i].products.distributor.level_four_price,product[i].products.distributor.level_two_price,product[i].products.distributor.level_one_price,product[i].products.distributor.level_three_price)}</span>
+											<span>${Sprice1(products[i].poducts.distributor.level_four_price,products[i].products.distributor.level_two_price,products[i].products.distributor.level_one_price,products[i].products.distributor.level_three_price)}</span>
 											<br>
 											<span>${LanguageHtml('待支付','Waiting for payment')}</span>
 										</p>

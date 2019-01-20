07a5eaaf19bd1487acade02e77d429b9a85097dc
coen <chen.bao@yn-pulse.com>
2019-01-20 10:18:07 +0800

    cc

--- htdocs/resources/views/web/personal.blade.php
+++ htdocs/resources/views/web/personal.blade.php
@@ -971,7 +971,7 @@
 									<div class="am-u-sm-8">
 										<p>${LanguageHtml(products[i].products.zn_name,products[i].products.en_name)}</p>
 									</div>
-									<div class="am-u-sm-4 myOderDetailText">
+									<div class="am-u-sm-2 myOderDetailText">
 										<p>数量：${products[i].count}
 											<br>
 											<span>单价：$${products[i].products.distributor.level_four_price}</span>

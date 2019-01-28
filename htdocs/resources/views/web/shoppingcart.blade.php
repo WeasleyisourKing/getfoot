a1a6e39c15a43ad3ea1e66d27fd258fa729f39f2
mac <mac@macdeMacBook-Air-5.local>
2019-01-28 11:23:10 +0800

    user

--- htdocs/resources/views/web/shoppingcart.blade.php
+++ htdocs/resources/views/web/shoppingcart.blade.php
@@ -327,7 +327,7 @@
                 </div>
                 <div class="am-u-sm-2 cartBut clearfloat">
 			        <div class="float_left increase but" style="cursor:pointer " onclick="addNumverProduct(${i})" >＋</div>
-                 		 <input class="productNumber myNumber number float_left " value="${product[i].count}">
+                 		 <input class="productNumber myNumber number float_left " style="width:60px" type="number" value="${product[i].count}">
 			        <div class="float_left reduce but" style="cursor:pointer "onclick="removeNumverProduct(${i})" >－</div>
             </div>
                 <div class="am-u-sm-2 cartCzBut">

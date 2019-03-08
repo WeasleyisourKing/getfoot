7b4982725fd9cafd63d10f59ad5d234bb9c62208
coen <chen.bao@yn-pulse.com>
2019-03-08 12:06:22 +0800

    cc

--- htdocs/resources/views/layouts/forget.blade.php
+++ htdocs/resources/views/layouts/forget.blade.php
@@ -1,5 +1,3 @@
 
-<text>
-    您的验证码为{{$number}},有效期为30分钟
-</text>
+<br>你好:<b>{{$name}}</b><br><br><text><br>   您的验证码为{{$number}},有效期为{{$expire}}分钟</text></p><p><br></p><p></p><p><br></p><p></p><p><br></p>
 

8167efc79f12d08bcc6c0dc13af30f326ed3dd0a
coen <chen.bao@yn-pulse.com>
2019-03-30 22:23:55 +0800

    cc

--- htdocs/resources/views/admin/count/count-user.blade.php
+++ htdocs/resources/views/admin/count/count-user.blade.php
@@ -177,6 +177,9 @@
                subtext: '',
                x: 'center'
            },
+           tooltip: {
+               trigger: 'axis'
+           },
             xAxis: {
                 type: 'category',
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
@@ -185,7 +188,7 @@
                 type: 'value'
             },
             series: [{
-
+                name: '数值',
                 data:{!! $count!!},
                 type: 'line'
             }]

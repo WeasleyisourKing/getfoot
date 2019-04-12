e0857e4296dd4b7a8f240db504353ef01f6a823d
kevin <mac@xishuobhihushuo.lan>
2019-04-12 14:17:56 +0800

    test

--- htdocs/resources/views/admin/layout/app.blade.php
+++ htdocs/resources/views/admin/layout/app.blade.php
@@ -104,7 +104,7 @@
 
 
 <body class="fixed-left">
-<?php echo $role ?>
+
 <!-- Begin page -->
 <div id="wrapper">
 
@@ -452,7 +452,7 @@
 
 <script>
     TableManageButtons.init();
-    console.log(1)
+    console.log(<?php echo AdminRoleModel ?>)
 </script>
 <script>
     $("#bulk-btn").click(function () {

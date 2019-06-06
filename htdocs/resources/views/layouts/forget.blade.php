c4afc6fa70da050a1cf816ce7298d886208773fd
xunan <93954345@qq.com>
2019-06-06 16:39:55 +0800

    2

--- htdocs/resources/views/layouts/forget.blade.php
+++
@@ -1,3 +0,0 @@
-
-<br>你好:<b>{{$name}}</b><br><br><text><br>   您的验证码为{{$number}},有效期为{{$expire}}分钟</text></p><p><br></p><p></p><p><br></p><p></p><p><br></p>
-
--- htdocs/resources/views/layouts/remind.blade.php
+++
@@ -1,9 +0,0 @@
-
-你好:<b>{{$name}}</b>
-</br>
-
-<text>
-    您在12BUY下的订单号为<b>{{$number}}</b>下的商品<b>{{$snapName}}</b>已经发货啦，感谢您对12BUY的支持。
-
-</text>
-
